457eaefef8acbd070ba22ba4eb1a15464fe1ff43
slou <slou@alcanada.com>
2020-07-10 17:27:47 -0400

    nothing

--- src/MainUI.js
+++ src/MainUI.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Schedule from "./schedule/schedule";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 class MainUI extends React.Component{
     constructor(props) {
@@ -17,7 +19,29 @@ class MainUI extends React.Component{
 
     render() {
         return (
-            <Schedule ts={this.state.timeSlots}/>
+            <div>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+
+                    </Grid>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6} spacing={2}>
+                            <Button variant={'contained'} color={'primary'}>{'< <'}</Button>
+                            <Button variant={'contained'} color={'primary'}>{'> >'}</Button>
+                        </Grid>
+                        <Grid container xs={6} spacing={2}>
+                            <Button variant={'contained'} color={'primary'}>{'Add new staff'}</Button>
+                            <Button variant={'contained'} color={'primary'}>{'Set staff time'}</Button>
+                        </Grid>`
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Schedule ts={this.state.timeSlots}/>
+                    </Grid>
+                </Grid>
+
+            </div>
+
         );
     }
 }
